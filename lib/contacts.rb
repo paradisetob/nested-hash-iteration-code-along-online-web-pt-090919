5ce49ffe59e4fd61aac0403f85cee33bddf6a5cc
tracey brimage <onelovenilynn@gmail.com>
2019-10-03 05:04:06 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -19,3 +19,11 @@ def remove_strawberry(contacts)
     binding.pry
   end
 end
+
+contacts.each do |person, contact_details_hash|
+  if person == "Freddy Mercury"
+    contact_details_hash.each do |attribute, data|
+      binding.pry
+    end
+  end
+end
